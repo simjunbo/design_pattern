9da78e9a9d3c71c0dac74adcfe7aad8548da1b26
simjunbo <s860620@gmail.com>
2019-02-11 21:14:11 +0900

    add

--- src/main/java/state2/context/Light.java
+++ src/main/java/state2/context/Light.java
@@ -4,6 +4,9 @@ import state2.concretestate.StateOff;
 import state2.concretestate.StateOn;
 import state2.state.State;
 
+/**
+ * 참고) https://sticky32.tistory.com/entry/%EC%86%8C%ED%94%84%ED%8A%B8%EC%9B%A8%EC%96%B4%EB%94%94%EC%9E%90%EC%9D%B8%ED%8C%A8%ED%84%B4-%EC%8A%A4%ED%85%8C%EC%9D%B4%ED%8A%B8-%ED%8C%A8%ED%84%B4State-Pattern
+ */
 public class Light {
 	private State state = new StateOff();
 
